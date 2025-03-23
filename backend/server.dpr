c0cd2eed61fe20ef80e8752db8c22dbdd0800c1d
Gustavo Sequeira <sequeira2003@gmail.com>
2025-03-23 11:05:46 -0300

    inclusao do endpoint tarefa

--- backend/server.dpr
+++ backend/server.dpr
@@ -8,15 +8,18 @@ uses
   System.SysUtils,
   Horse,
   Horse.Jhonson,
-  Usuario.Controller in 'Usuario.Controller.pas',
+  Usuario.Controller in 'controller\Usuario.Controller.pas',
   Usuario.Model in 'model\Usuario.Model.pas',
   Config.Database in 'Config.Database.pas',
   System.JSON,
-  Tarefa.Model in 'model\Tarefa.Model.pas';
+  Tarefa.Model in 'model\Tarefa.Model.pas',
+  Tarefa.Controller in 'controller\Tarefa.Controller.pas';
 
 begin
   THorse.Use(Jhonson());
+
   Usuario.Controller.Registry;
+  Tarefa.Controller.Registry;
 
   THorse.Listen(9000);
 
